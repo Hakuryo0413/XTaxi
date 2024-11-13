d478ac95def92fb846dc6726b40c7758c602b5b8
21020410-NguyenThinhThuan <21020410@vnu.edu.vn>
2024-11-13 21:18:15 +0700

    fixed footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ const currentYear = new Date().getFullYear();
 
 export default function UserSideFooter() {
   return (
-    <footer className="w-full bg-background mt-8 text-white">
+    <footer className="w-full bg-primary text-white">
       <div className="mx-auto w-full max-w-7xl px-8">
         <div className="grid  grid-cols-1 justify-between gap-4 md:grid-cols-2">
           <Typography variant="h5" className="mb-6 text-textColor">
