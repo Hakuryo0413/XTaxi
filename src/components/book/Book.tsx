aaeed4de0a80c61dfbafb452a675041940fde232
21020410-NguyenThinhThuan <21020410@vnu.edu.vn>
2024-11-21 21:18:48 +0700

    update BookingPage

--- src/components/book/Book.tsx
+++ src/components/book/Book.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import './Book.css';
 import MapComponent from './MapComponent';
 
+import imgTaxi1 from '@src/image/sedan10.png';
+import imgTaxi2 from '@src/image/van10.png';
 
+/*7f3JGEevFUfR7Og8SvonyvJym2HAzhFF*/
 const Book: React.FC = () => {
   return (
-    <div style={{ height: '100vh', width: '100%' }}>
-      <MapComponent />
+    <div className="book-container">
+      <div className="book-left">
+        <MapComponent />
+      </div>
+      <div className="book-right">
+        <div className="book-right-top">
+          <div className="book-right-item">
+            <img src={imgTaxi1} alt="Image 1" className="item-image" />
+            <p className="item-text">10.000đ / Km</p>
+            <p className="item-text"> 1-4 passengers</p>
+            <button className="item-button">CHOOSE</button>
+          </div>
+          <div className="book-right-item">
+            <img src={imgTaxi2} alt="Image 2" className="item-image" />
+            <p className="item-text">15.000đ / Km</p>
+            <p className="item-text"> 4-7 passengers</p>
+            <button className="item-button">CHOOSE</button>
+          </div>
+        </div>
+        <div className="book-right-bottom">
+          <div className="book-right-item">
+            <p className="item-text">FARE: </p>
+            <button className="item-button">BOOK</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
