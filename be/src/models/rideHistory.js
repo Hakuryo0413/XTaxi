e36627155f30c2571f22d30b0ab1390fa8978896
tram.tth <tram.tth@teko.vn>
2024-11-22 23:49:20 +0700

    Add be folder

---
+++ be/src/models/rideHistory.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const rideHistorySchema = new Schema({
+  user_id: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  ride_id: {
+    type: Schema.Types.ObjectId,
+    ref: "Ride",
+    required: true,
+  },
+  driver_id: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["completed", "canceled"],
+    required: true,
+  },
+  start_time: {
+    type: Date,
+    required: true,
+  },
+  end_time: Date,
+  fare: {
+    type: Number,
+    required: true,
+  },
+  user_rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+  },
+  driver_feedback: String,
+});
+const RideHistory = mongoose.model('RideHistory', rideHistorySchema);
+module.exports = RideHistory;
+
